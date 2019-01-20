86da789a3ba59a978655189f52622ee0e3543e08
zmy <1120173455@bit.edu.cn>
2019-01-20 19:48:34 +0800

    Initial commit

---
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/model/Message.java
@@ -0,0 +1,92 @@
+package chapter.android.aweme.ss.com.homework.model;
+import android.graphics.drawable.Drawable;
+
+import android.media.Image;
+
+import android.util.Log;
+
+
+
+import chapter.android.aweme.ss.com.homework.R;
+
+/**
+ * 消息  data class
+ */
+public class Message {
+
+    private boolean isOfficial;
+    private String title;
+    private String time;
+    private String description;
+    private String icon;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public String getIcon() {
+//        return icon;
+//    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public boolean isOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(boolean official) {
+        isOfficial = official;
+    }
+    public int getIcon() {
+
+        Log.d("Icon",icon);
+
+        if(icon.equals("TYPE_ROBOT")) return R.drawable.session_robot;
+
+        if(icon.equals("TYPE_GAME") ) return R.drawable.icon_micro_game_comment;
+
+        if(icon.equals("TYPE_SYSTEM")) return R.drawable.session_system_notice;
+
+        if(icon.equals("TYPE_STRANGER")) return R.drawable.session_stranger;
+
+        if(icon.equals("TYPE_USER")) return R.drawable.icon_girl;
+
+        return R.drawable.session_stranger;
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "isOfficial=" + isOfficial +
+                ", title='" + title + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", icon='" + icon + '\'' +
+                '}';
+    }
+}
